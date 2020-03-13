2ba55f7913233c3fea3d70129208332f6a855d28
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-03-13 18:46:48 +0100

    Added Migrations and Entity framework database

--- BookListRazor/Model/ApplicationDbContext.cs
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace BookListRazor.Model
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
     }
 }
--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -11,6 +11,7 @@ namespace BookListRazor.Model
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public string Author { get; set; } 
